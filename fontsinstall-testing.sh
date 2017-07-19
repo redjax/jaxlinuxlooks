50bf7f2f25d67d575212304a847910b716dec7b1
Jack Kenyon <jackenyon@gmail.com>
2017-07-19 01:04:00 -0400

    Fonts 2
    Added verbose and progress flags to git clone ops

--- fontsinstall-testing.sh
+++ fontsinstall-testing.sh
@@ -3,19 +3,19 @@
 # Fonts install
 
 # Hasklig
-git clone https://github.com/i-tu/Hasklig.git /home/jack/Downloads/test//
+git clone --progress --verbose https:/github.com/i-tu/Hasklig.git /home/jack/Downloads/test/
 
 # DejaVu Sans Code
-git clone https://github.com/SSNikolaevich/DejaVuSansCode /home/jack/Downloads/test//
+git clone --progress --verbose https:/github.com/SSNikolaevich/DejaVuSansCode /home/jack/Downloads/test/
 
 # Adobe Source Code Pro
-git clone https://github.com/adobe-fonts/source-code-pro.git /home/jack/Downloads/test//
+git clone --progress --verbose https:/github.com/adobe-fonts/source-code-pro.git /home/jack/Downloads/test/
 
 # Fira Code
-git clone https://github.com/tonsky/FiraCode.git /home/jack/Downloads/test//
+git clone --progress --verbose https:/github.com/tonsky/FiraCode.git /home/jack/Downloads/test/
 
 # Droid Sans Mono
-git clone https://github.com/AlbertoDorado/droid-sans-mono-zeromod.git /home/jack/Downloads/test//
+git clone --progress --verbose https:/github.com/AlbertoDorado/droid-sans-mono-zeromod.git /home/jack/Downloads/test/
 
 # Google Fonts
-git clone https://github.com/google/fonts.git /home/jack/Downloads/test//
+git clone --progress --verbose https:/github.com/google/fonts.git /home/jack/Downloads/test/
--- themesinstall.sh
+++ themesinstall.sh
@@ -6,7 +6,7 @@
 #dnf copr enable tcg/themes
 #dnf install la-capitaine-cursor-theme
 
-git clone https://github.com/keeferrourke/capitaine-cursors.git ~/home/Downloads/la-capitaine-cursor
+git clone --progress --verbose https://github.com/keeferrourke/capitaine-cursors.git ~/home/Downloads/la-capitaine-cursor
 cp -pr ~/home/Downloads/la-capitaine-cursor-theme/bin/xcursor /usr/share/icons/capitaine-cursors
 
 # ----------------------------------------------------------------------------------------
@@ -17,37 +17,37 @@ cp -pr ~/home/Downloads/la-capitaine-cursor-theme/bin/xcursor /usr/share/icons/c
 gsettings set  org.gnome.desktop.wm.preferences button-layout 'close,minimize,maximize:'
 
 # Xenlism Minimalism
-git clone https://github.com/xenlism/minimalism /usr/share/themes/Xenlism-Minimalism
+git clone --progress --verbose https://github.com/xenlism/minimalism /usr/share/themes/Xenlism-Minimalism
 
 # Flat Remix
-git clone https://github.com/daniruiz/Flat-Remix-GNOME-theme.git /usr/share/themes/Flat-Remix
+git clone --progress --verbose https://github.com/daniruiz/Flat-Remix-GNOME-theme.git /usr/share/themes/Flat-Remix
 
 # Arc
-git clone https://github.com/horst3180/arc-theme.git /usr/share/themes/Arc
+git clone --progress --verbose https://github.com/horst3180/arc-theme.git /usr/share/themes/Arc
 
 # Gnome-OSX
-git clone https://github.com/paullinuxthemer/Gnome-OSX.git /usr/share/themes/Gnome-OSX
+git clone --progress --verbose https://github.com/paullinuxthemer/Gnome-OSX.git /usr/share/themes/Gnome-OSX
 
 # Minwaita
-git clone https://github.com/godlyranchdressing/Minwaita.git /usr/share/themes/Minwaita
+git clone --progress --verbose https://github.com/godlyranchdressing/Minwaita.git /usr/share/themes/Minwaita
 
 # Arc Menu
-git clone https://github.com/LinxGem33/Arc-Menu.git ~/Downloads/Arc-Menu
+git clone --progress --verbose https://github.com/LinxGem33/Arc-Menu.git ~/Downloads/Arc-Menu
 cd ~/Downloads/Arc-Menu
 make install
 make enable
 
 # OSX-Arc-Darker
-git clone https://github.com/LinxGem33/OSX-Arc-Darker.git /usr/share/themes/OSX-Arc-Darker
+git clone --progress --verbose https://github.com/LinxGem33/OSX-Arc-Darker.git /usr/share/themes/OSX-Arc-Darker
 
 # OSX-Arc-Shadow
-git clone https://github.com/LinxGem33/OSX-Arc-Shadow.git /usr/share/themes/OSX-Arc-Shadow
+git clone --progress --verbose https://github.com/LinxGem33/OSX-Arc-Shadow.git /usr/share/themes/OSX-Arc-Shadow
 
 # OSX-Arc-White
-git clone https://github.com/LinxGem33/OSX-Arc-White.git /usr/share/themes/OSX-Arc-White
+git clone --progress --verbose https://github.com/LinxGem33/OSX-Arc-White.git /usr/share/themes/OSX-Arc-White
 
 # OSX-Arc-Plus
-git clone https://github.com/LinxGem33/OSX-Arc-Plus.git /usr/share/themes/OSX-Arc-Plus
+git clone --progress --verbose https://github.com/LinxGem33/OSX-Arc-Plus.git /usr/share/themes/OSX-Arc-Plus
 
 # ----------------------------------------------------------------------------------------
 
@@ -57,51 +57,51 @@ git clone https://github.com/LinxGem33/OSX-Arc-Plus.git /usr/share/themes/OSX-Ar
 cp sublime-text.png /usr/share/icons/
 
 # Arc-OSX-Icons
-git clone https://github.com/LinxGem33/Arc-OSX-Icons.git /usr/share/icons/Arc-OSX-Icons
+git clone --progress --verbose https://github.com/LinxGem33/Arc-OSX-Icons.git /usr/share/icons/Arc-OSX-Icons
 cd /usr/share/themes/Arc-OSX-Icons/src/
 cp -R * /usr/share/themes/
 
 # Plane icon theme
-git clone https://github.com/wfpaisa/plane-icon-theme.git /usr/share/icons/Plane
+git clone --progress --verbose https://github.com/wfpaisa/plane-icon-theme.git /usr/share/icons/Plane
 cd /usr/share/Plane
 cp -R src/* ../
 
 # Evolvere
-git clone https://github.com/franksouza183/EvolvereSuit.git /usr/share/icons/Evolere
+git clone --progress --verbose https://github.com/franksouza183/EvolvereSuit.git /usr/share/icons/Evolere
 
 # Luv Icons
-git clone https://github.com/Nitrux/luv-icon-theme.git /usr/share/icons/Luv-Icons
+git clone --progress --verbose https://github.com/Nitrux/luv-icon-theme.git /usr/share/icons/Luv-Icons
 cd /usr/share/icons/Luv-Icons
 cp -R Luv ../Luv
 
 # Matrilineare
-git clone https://github.com/n3storm/Matrilineare.git /usr/share/icons/Matrilineare
+git clone --progress --verbose https://github.com/n3storm/Matrilineare.git /usr/share/icons/Matrilineare
 
 # Xenlism Wildfire
-git clone https://github.com/xenlism/wildfire.git /usr/share/icons/Xenlism-Wildfire
+git clone --progress --verbose https://github.com/xenlism/wildfire.git /usr/share/icons/Xenlism-Wildfire
 cd /usr/share/icons/Xenlism-Wildfire/INSTALL
 ./git.install
 
 # Obsidian
-git clone https://github.com/madmaxms/iconpack-obsidian.git /usr/share/icons/Obsidian
+git clone --progress --verbose https://github.com/madmaxms/iconpack-obsidian.git /usr/share/icons/Obsidian
 
 # Lila HD
-git clone https://github.com/ilnanny/Lila-HD-icon-theme.git /usr/share/icons/Lila-HD
+git clone --progress --verbose https://github.com/ilnanny/Lila-HD-icon-theme.git /usr/share/icons/Lila-HD
 cd /usr/share/icons/Lila-HD
 ./install.sh
 
 # Emerald
-git clone https://github.com/vinceliuice/emerald-icon-theme.git /usr/share/icons/Emerald
+git clone --progress --verbose https://github.com/vinceliuice/emerald-icon-theme.git /usr/share/icons/Emerald
 cd /usr/share/icons/Emerald
 ./Emerald-installer.sh
 
 # Paper
-git clone https://github.com/snwh/paper-icon-theme.git /usr/share/icons/Paper
+git clone --progress --verbose https://github.com/snwh/paper-icon-theme.git /usr/share/icons/Paper
 cd /usr/share/icons/Paper
 cp -R Paper ../Paper-Icons
 
 # Flat Remix
-git clone https://github.com/daniruiz/Flat-Remix.git /usr/share/icons/Flat-Remix
+git clone --progress --verbose https://github.com/daniruiz/Flat-Remix.git /usr/share/icons/Flat-Remix
 cd /usr/share/icons/Flat-Remix
 cp -R Flat\ Remix ../Flat\ Remix
 
